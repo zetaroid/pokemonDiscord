1fe53d5e059e232ba35327242f30a9203173d1a4
Marcus <mlo11@duke.edu>
2021-02-13 23:09:19 -0800

    buy money with bp

--- main.py
+++ main.py
@@ -2424,7 +2424,8 @@ async def startMartUI(ctx, trainer, goBackTo='', otherData=None):
             "Masterball": 50,
             "Full Restore": 1,
             "Full Heal": 1,
-            "Max Revive": 1
+            "Max Revive": 1,
+            "PokeDollars x3000": 1
         }
     if (trainer.checkFlag("badge5")):
         itemDict = itemsForPurchase3
@@ -2534,7 +2535,7 @@ async def startMartUI(ctx, trainer, goBackTo='', otherData=None):
                             trainer.addItem(key, 1)
                             # print("mart: " + trainer.name + "bought " + key + " and now has a total of " + str(trainer.getItemAmount(key)))
                             embed.set_footer(text="BP: " + str(trainer.getItemAmount('BP'))
-                                                  + "\nBought 1x " + key + " for " + str(itemDict[key]) + " BP.")
+                                              + "\nBought 1x " + key + " for " + str(itemDict[key]) + " BP.")
                             await message.edit(embed=embed)
                     else:
                         if (trainer.getItemAmount('money') >= itemDict[key]):
@@ -2548,10 +2549,15 @@ async def startMartUI(ctx, trainer, goBackTo='', otherData=None):
                     if trainer.location == "Battle Frontier":
                         if (trainer.getItemAmount('BP') >= itemDict[key]):
                             trainer.addItem('BP', -1 * itemDict[key])
-                            trainer.addItem(key, 1)
-                            # print("mart: " + trainer.name + "bought " + key + " and now has a total of " + str(trainer.getItemAmount(key)))
-                            embed.set_footer(text="BP: " + str(trainer.getItemAmount('BP'))
-                                                  + "\nBought 1x " + key + " for " + str(itemDict[key]) + " BP.")
+                            if key == "PokeDollars x3000":
+                                trainer.addItem('money', 3000)
+                                embed.set_footer(text="BP: " + str(trainer.getItemAmount('BP'))
+                                                      + "\nBought $3000 for " + str(itemDict[key]) + " BP.")
+                            else:
+                                trainer.addItem(key, 1)
+                                # print("mart: " + trainer.name + "bought " + key + " and now has a total of " + str(trainer.getItemAmount(key)))
+                                embed.set_footer(text="BP: " + str(trainer.getItemAmount('BP'))
+                                                      + "\nBought 1x " + key + " for " + str(itemDict[key]) + " BP.")
                             await message.edit(embed=embed)
                     else:
                         if (trainer.getItemAmount('money') >= itemDict[key]):
