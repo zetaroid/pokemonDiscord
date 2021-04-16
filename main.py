ee47fef51b0ee5f8defb702e2366da0427212dc3
Marcus <mlo11@duke.edu>
2021-04-16 01:02:32 -0700

    log instead of send to discord

--- main.py
+++ main.py
@@ -82,7 +82,8 @@ async def forbiddenErrorHandle(ctx):
         channel = await ctx.author.create_dm()
         await channel.send(forbiddenMessage)
     disregardStr = "Error for '" + str(ctx.author.id) + "' was due to missing permissions. You can safely disregard."
-    await sendDiscordErrorMessage(ctx, traceback, disregardStr)
+    logging.error(str(ctx.author.id) + " - " + disregardStr)
+    # await sendDiscordErrorMessage(ctx, traceback, disregardStr)
 
 async def sessionErrorHandle(ctx, user, traceback):
     logging.error(str(ctx.author.id) + "'s session ended in error.\n" + str(traceback.format_exc()) + "\n")
